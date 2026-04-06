cce4cf64378968d5e2559eee8bf05bbd097a83be
alexispoliti <walexispoliti@gmail.com>
2026-04-06 20:50:02 -0300

    subidaPoliti

---
+++ punto1.py
@@ -0,0 +1,12 @@
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def eje_x(self):
+        return self.eje_x
+    
+    def eje_y(self):
+        return self.eje_y
+    
+    
